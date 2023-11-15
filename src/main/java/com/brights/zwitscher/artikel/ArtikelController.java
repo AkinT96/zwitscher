adc02e14d0eb62bbd31cdbb13b550c4289bca493
Akin <akin-tas96@hotmail.de>
2023-11-15 16:00:12 +0100

    Artikelliste hinzugefügt und einen beispielartikel erstellt

---
+++ src/main/java/com/brights/zwitscher/artikel/ArtikelController.java
@@ -0,0 +1,25 @@
+package com.brights.zwitscher.artikel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArtikelController {
+
+    private ArtikelRepository artikelRepository;
+
+    @Autowired
+    public ArtikelController(ArtikelRepository artikelRepository) {
+        this.artikelRepository = artikelRepository;
+    }
+
+    @GetMapping("/posts")
+    public ArtikelListeDTO artikelausgeben() {
+        List<Artikel> artikelListe = artikelRepository.findAll();
+        return new ArtikelListeDTO(artikelListe);
+    }
+}
