f18c9f0203d2191279ba8f8dc0c67de7f8e7c60f
Sebastian Kluge <sebastian.kluge@posteo.de>
2023-11-16 21:56:25 +0100

    delete hinzugefügt

--- src/main/java/com/brights/zwitscher/admin/AdminController.java
+++ src/main/java/com/brights/zwitscher/admin/AdminController.java
@@ -1,10 +1,12 @@
 package com.brights.zwitscher.admin;
 
+import com.brights.zwitscher.artikel.Artikel;
 import com.brights.zwitscher.artikel.ArtikelRepository;
 import com.brights.zwitscher.kommentare.KommentarRepository;
 import com.brights.zwitscher.user.User;
 import com.brights.zwitscher.user.UserDTO;
 import com.brights.zwitscher.user.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -30,8 +32,23 @@ public class AdminController {
     public User alsAdminSetzen(@PathVariable Long userId, @ModelAttribute("sessionUser") Optional<User> sessionUserOptional){
         User user = sessionUserOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Logindaten sind nicht gültig."));
         if (!user.isAdmin()) throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Du bist kein Admin");
-        User userAlsAdmin = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "Der Benutzername existiert nicht."));
+        User userAlsAdmin = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Der Benutzername existiert nicht."));
         userAlsAdmin.setAdmin(true);
         return userRepository.save(userAlsAdmin);
-          }
+    }
+    // TODO check cascading when i delete the artikel, the comments should be removed too
+    @DeleteMapping("/lösche/artikel/{artikelId}")
+    public void artikelLöschen (@PathVariable Long artikelId, @ModelAttribute("sessionUser") Optional<User> sessionUserOptional,
+                                HttpServletResponse response){
+
+        User user = sessionUserOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                "Logindaten sind nicht gültig."));
+        if (!user.isAdmin()) throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Du bist kein Admin");
+        Artikel artikel = artikelRepository.findById(artikelId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Der Artikel existiert nicht."));
+        artikelRepository.delete(artikel);
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT); // status code 204, best practice
+
+    }
+
 }
