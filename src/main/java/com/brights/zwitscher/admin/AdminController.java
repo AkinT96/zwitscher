0d0ad8012895f036847a5e95ee7c995556180851
Akin <akin-tas96@hotmail.de>
2023-11-16 22:39:20 +0100

    UserIdDTO.java hinzugefügt und benutzerliste.html bearbeitet

--- src/main/java/com/brights/zwitscher/admin/AdminController.java
+++ src/main/java/com/brights/zwitscher/admin/AdminController.java
@@ -4,6 +4,7 @@ import com.brights.zwitscher.artikel.ArtikelRepository;
 import com.brights.zwitscher.kommentare.KommentarRepository;
 import com.brights.zwitscher.user.User;
 import com.brights.zwitscher.user.UserDTO;
+import com.brights.zwitscher.user.UserIdDTO;
 import com.brights.zwitscher.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,12 +30,12 @@ public class AdminController {
         this.kommentarRepository = kommentarRepository;
     }
 
-    @GetMapping("/setzealsadmin/{userId}")
-    public User alsAdminSetzen(@PathVariable Long userId, @ModelAttribute("sessionUser") Optional<User> sessionUserOptional){
+    @PostMapping("/setzeAlsAdmin")
+    public User alsAdminSetzen(@RequestBody UserIdDTO userIdDTO, @ModelAttribute("sessionUser") Optional<User> sessionUserOptional){
         User user = sessionUserOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Logindaten sind nicht gültig."));
         if (!user.isAdmin()) throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Du bist kein Admin");
 
-        User userAlsAdmin = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "Der Benutzername existiert nicht."));
+        User userAlsAdmin = userRepository.findById(userIdDTO.getUserId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "Der Benutzername existiert nicht."));
         userAlsAdmin.setAdmin(true);
         return userRepository.save(userAlsAdmin);
           }
