24b7923fa1b1da3a72a6c56002bf92f0491c6b7a
Akin <akin-tas96@hotmail.de>
2023-11-18 20:14:00 +0100

    edwards änderungen

---
+++ src/main/resources/static/indexScript.js
@@ -0,0 +1,29 @@
+document.addEventListener('htmx:load', function (event) {
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.loginLink');
+    const registrierenLink = document.querySelector('.registrierenLink');
+    const btnNavLogin = document.querySelector('.btnNavLogin');
+    const boxSchließen = document.querySelector('.boxSchließen');
+
+    if (registrierenLink && wrapper) {
+        registrierenLink.addEventListener('click', () => {
+            console.log('Link wurde geklickt');
+            wrapper.classList.add('active');
+        });
+    }
+
+    if (loginLink && wrapper) {
+        loginLink.addEventListener('click', ()=> {
+            wrapper.classList.remove('active');
+        })
+    }
+
+    btnNavLogin.addEventListener('click', ()=> {
+        wrapper.classList.add('active-popup');
+    });
+
+    boxSchließen.addEventListener('click', ()=> {
+        wrapper.classList.remove('active-popup');
+    });
+
+});
